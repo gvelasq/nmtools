ad8b89a5345219d7b86c66ade5233d3ebcb26574
Gustavo Velásquez <gvelasq2github@gmail.com>
2019-01-10 10:14:52 -0500

    Add global variables

--- R/nmtools-package.R
+++ R/nmtools-package.R
@@ -1,2 +1,16 @@
 #' @keywords internal
 "_PACKAGE"
+
+globalVariables(
+  c(
+    "covariance_step_successful",
+    "covariance_step_warnings",
+    "estimate_near_boundary",
+    "folder",
+    "folder_path",
+    "minimization_successful",
+    "model",
+    "path_exists",
+    "sd"
+  )
+)
